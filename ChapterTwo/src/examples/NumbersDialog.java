ba80724176e67a333eafec362f8a4d54ebc96fb0
RisenMatyr <RisenMatyr@gmail.com>
2017-03-30 12:06:54 -0500

    Update

---
+++ ChapterTwo/src/examples/NumbersDialog.java
@@ -0,0 +1,13 @@
+package examples;
+import javax.swing.JOptionPane;
+public class NumbersDialog 
+{
+
+	public static void main(String[] args) 
+	{
+		 int creditDays = 30;
+		 JOptionPane.showMessageDialog(null,"" + creditDays);
+		 JOptionPane.showMessageDialog(null,"Every bill is due in " + creditDays + " days.");
+	}
+
+}
