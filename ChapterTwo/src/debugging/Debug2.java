e6b75345d1986406a58e701697fbf0c972ba9bea
RisenMatyr <RisenMatyr@gmail.com>
2017-04-05 11:02:17 -0500

    Update

---
+++ ChapterTwo/src/debugging/Debug2.java
@@ -0,0 +1,15 @@
+package debugging;
+
+public class Debug2 
+{
+		// TODO Auto-generated method stub
+		   public static void main(String args[])
+		   {
+		      int a, b;
+		      a = 7;
+		      b = 4;
+		      System.out.println("The sum is " + (a + b));
+		      System.out.println("The difference is " + (a - b));
+		      System.out.println("The product is " + (a * b));
+		   }
+}
